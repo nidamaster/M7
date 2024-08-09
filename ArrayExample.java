38a64eb4d088d0c4db816f47ed95470ecb7049af
nidamaster <160611221+nidamaster@users.noreply.github.com>
2024-08-09 14:38:40 +0530

    array example

---
+++ ArrayExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class 
+{
+	public static void main(String[] args)
+	{
+		int[] marks = new int[2];
+		marks[0] = 50;
+		marks[1] = 40;
+		// System.out.println(marks[0]);
+		// System.out.println(marks[1]);
+
+		for(int i=0; i<3; i++)
+			System.out.println(marks[i]);
+		
+	}
+}
